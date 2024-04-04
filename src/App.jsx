2a3f2af012be84ffe4f5039d4e7389b75edbe695
Daniel Gallardo Martos <dangalmar@alum.us.es>
2024-04-04 13:40:37 +0200

    feat: Añadido funcionalidad de filtrado de personajes

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,62 @@
 import imageRickMorty from "./assets/img/rick-morty.png";
 import "./App.css";
 import Characters from "./components/Characters";
+import FilterForm from "./components/FilterForm";
 import { useState } from "react";
 
 function App() {
   const [characters, setCharacters] = useState(null);
+  const [error, setError] = useState(false);
+  const [filters, setFilters] = useState({ name: "", status: "", species: "", type: "", gender: "" });
+
+  const handleFilterChange = (e) => {
+    const { name, value } = e.target;
+    setFilters((prevFilters) => ({
+      ...prevFilters,
+      [name]: value
+    }));
+  };
+
 
   const reqApi = async () => {
-    const api = await fetch("https://rickandmortyapi.com/api/character");
+    let apiUrl = "https://rickandmortyapi.com/api/character";
+    const filterParams = new URLSearchParams(filters).toString();
+    if (filterParams) {
+      apiUrl += `?${filterParams}`;
+    }
+
+    const api = await fetch(apiUrl);
     const characterApi = await api.json();
 
-    setCharacters(characterApi.results);
+    if (characterApi.results != undefined) {
+      setError(false)
+      setCharacters(characterApi.results);
+    } else {
+      setError(true);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    reqApi();
   };
 
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="title">Rick & Morty</h1>
-        {characters ? (
-          <Characters characters={characters} setCharacters={setCharacters}/>
+        {error ? (
+          <>
+            <p className="text-red-500">No characters was found</p>
+            <FilterForm filters={filters} handleFilterChange={handleFilterChange} handleSubmit={handleSubmit} />
+            <Characters characters={characters} setCharacters={setCharacters}/>
+          </>
+          
+        ) :characters ? (
+          <>
+            <FilterForm filters={filters} handleFilterChange={handleFilterChange} handleSubmit={handleSubmit} />
+            <Characters characters={characters} setCharacters={setCharacters}/>
+          </>
         ) : (
           <>
             <img src={imageRickMorty} alt="Rick & Morty" className="img-home" />
